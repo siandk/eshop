f6742dfbcc50054ab3659ec95dbb34ada1fcd7e1
Simon Andersen <simon.andersen@protonmail.com>
2019-04-01 06:31:52 +0200

    Add parentpath to category and more fields on products

---
+++ EShop/DataLayer/Migrations/20190330070534_CategoryParentPath.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class CategoryParentPath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ParentPath",
+                table: "Categories",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ParentPath",
+                table: "Categories");
+        }
+    }
+}
---
+++ EShop/DataLayer/Migrations/20190330162039_FeaturedProducts.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataLayer.Migrations
+{
+    public partial class FeaturedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                maxLength: 250,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 150,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Featured",
+                table: "Products",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Summary",
+                table: "Products",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Featured",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Summary",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 250,
+                oldNullable: true);
+        }
+    }
+}
