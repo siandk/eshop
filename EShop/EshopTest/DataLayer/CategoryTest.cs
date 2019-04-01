0591e282c8dc62d57530636949978d1fc397a29f
Simon Andersen <simon.andersen@protonmail.com>
2019-04-01 06:31:09 +0200

    Start work on tests

---
+++ EShop/EshopTest/DataLayer/CategoryTest.cs
@@ -0,0 +1,82 @@
+﻿using DataLayer;
+using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EshopTest.DataLayer
+{
+    [TestClass]
+    public class CategoryTest
+    {
+        [TestMethod]
+        public void set_category_parentpath()
+        {
+            // ARRANGE
+            var options = new DbContextOptionsBuilder<ShopContext>()
+                .UseInMemoryDatabase(databaseName: "set_category_parentpath")
+                .Options;
+
+            // ACT
+            using (var context = new ShopContext(options))
+            {
+                List<Category> categoryList = new List<Category>()
+                {
+                    new Category() {Name = "Layer 0"},
+                    new Category() {Name = "Layer 1", ParentCategoryId = 1},
+                    new Category() {Name = "Layer 2", ParentCategoryId = 2},
+                    new Category() {Name = "Layer 3", ParentCategoryId = 3}
+                };
+                context.Categories.AddRange(categoryList);
+                context.SaveChanges();
+
+            // ASSERT
+                Category category = context.Categories.Include(c => c.ChildCategories).Where(c => c.ParentCategoryId == 3).First();
+                Assert.AreEqual("Layer 3", category.Name); // Check that the correct category has been fetched
+                Assert.AreEqual("/1/2/3/", category.ParentPath); // Check that the parentpath has been set correctly
+            }
+        }
+        [TestMethod]
+        public void get_products_by_category_parentpath()
+        {
+            // ARRANGE
+            var options = new DbContextOptionsBuilder<ShopContext>()
+                .UseInMemoryDatabase(databaseName: "get_products_by_category_parentpath")
+                .Options;
+            
+            using (var context = new ShopContext(options))
+            {
+                List<Category> categoryList = new List<Category>()
+                {
+                    new Category() {Name = "Layer 0"},
+                    new Category() {Name = "Layer 1", ParentCategoryId = 1},
+                    new Category() {Name = "Layer 2", ParentCategoryId = 2},
+                    new Category() {Name = "Layer 2.A", ParentCategoryId = 2},
+                    new Category() {Name = "Layer 3", ParentCategoryId = 3}
+                };
+                context.Categories.AddRange(categoryList);
+
+                List<Product> productList = new List<Product>()
+                {
+                    new Product() {Name = "Test Product 01", CategoryId = 1, UnitPrice = 30M},
+                    new Product() {Name = "Test Product 02", CategoryId = 1, UnitPrice = 30M},
+                    new Product() {Name = "Test Product 03", CategoryId = 2, UnitPrice = 30M},
+                    new Product() {Name = "Test Product 04", CategoryId = 2, UnitPrice = 30M},
+                    new Product() {Name = "Test Product 05", CategoryId = 3, UnitPrice = 30M},
+                    new Product() {Name = "Test Product 06", CategoryId = 3, UnitPrice = 30M},
+                    new Product() {Name = "Test Product 07", CategoryId = 4, UnitPrice = 30M},
+                    new Product() {Name = "Test Product 08", CategoryId = 5, UnitPrice = 30M}
+                };
+                context.Products.AddRange(productList);
+                context.SaveChanges();
+
+                // ACT
+                var categories = context.Categories.Include(c => c.ChildCategories).Include(c => c.Products).Where(c => c.ParentPath != null && c.ParentPath.Contains("/2/")).ToList();
+                var products = categories.SelectMany(c => c.Products).ToList();
+                Assert.AreEqual(4, products.Count());
+            }
+
+        }
+    }
+}
---
+++ EShop/EshopTest/ServiceLayer/CategoryServiceTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer;
+using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServiceLayer.ShopService.Concrete;
+using System.Linq;
+
+namespace EshopTest.ServiceLayer
+{
+    [TestClass]
+    public class CategoryServiceTest
+    {
+        [TestMethod]
+        public void get_category_hierarchy()
+        {
+            // ARRANGE
+            var options = new DbContextOptionsBuilder<ShopContext>()
+                .UseInMemoryDatabase(databaseName: "set_category_parentpath")
+                .Options;
+            using (var context = new ShopContext(options))
+            {
+                List<Category> categoryList = new List<Category>()
+                {
+                    new Category() {Name = "Layer 0"},
+                    new Category() {Name = "Layer 1", ParentCategoryId = 1},
+                    new Category() {Name = "Layer 1.A", ParentCategoryId = 1},
+                    new Category() {Name = "Layer 1.B", ParentCategoryId = 1},
+                    new Category() {Name = "Layer 2", ParentCategoryId = 2},
+                    new Category() {Name = "Layer 3", ParentCategoryId = 5}
+                };
+                context.Categories.AddRange(categoryList);
+                context.SaveChanges();
+
+                // ACT
+                CategoryService _service = new CategoryService(context);
+                var categoryDtos = _service.GetCategoryTree().ToList();
+                // ASSERT
+                Assert.AreEqual(6, categoryDtos.Count()); // All categories loaded
+                Assert.AreEqual(3, categoryDtos.Where(c => c.ParentCategoryId == null).SelectMany(c => c.ChildCategories).Count()); // 3 children directly under top level
+                Assert.AreEqual(1, categoryDtos.First().ChildCategories.First().ChildCategories.First().ChildCategories.Count()); // Nested categories are also loaded
+            }
+        }
+    }
+}
