a0ea54924df79ef524be05b5b885dfc6e3c0b09c
Simon Andersen <simon.andersen@protonmail.com>
2019-04-08 15:10:35 +0200

    Add orders to admin section

---
+++ EShop/EshopClient/Pages/Admin/Orders/Edit.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ServiceLayer.ShopService.Dto;
+using ServiceLayer.ShopService.Interfaces;
+
+namespace EshopClient.Pages.Admin.Orders
+{
+    public class EditModel : PageModel
+    {
+
+        private readonly IOrderService _service;
+
+        public EditModel(IOrderService service)
+        {
+            _service = service;
+        }
+        public OrderDto Order { get; set; }
+        public async Task OnGetAsync(int orderId)
+        {
+            Order = await _service.GetOrderById(orderId).FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ EShop/EshopClient/Pages/Admin/Orders/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ServiceLayer.ShopService.Dto;
+using ServiceLayer.ShopService.Interfaces;
+
+namespace EshopClient.Pages.Admin.Orders
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IOrderService _service;
+
+        public IndexModel(IOrderService service)
+        {
+            _service = service;
+        }
+        public List<OrderDto> Orders { get; set; }
+        public async Task OnGetAsync()
+        {
+            Orders = await _service.GetOrders().ToListAsync();
+        }
+    }
+}
