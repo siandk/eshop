a0de6a6f591828051dace1796325ca8ec5f79507
Simon Andersen <simon.andersen@protonmail.com>
2019-04-25 08:28:46 +0200

    Separate modal features into new component

---
+++ eshop-spa/src/components/Modal.vue
@@ -0,0 +1,99 @@
+<template>
+
+  <transition>
+    <div class="vue-modal-mask">
+      <div class="vue-modal-wrapper">
+        <div class="vue-modal-container">
+
+            <div class="vue-modal-header">
+                <slot name="header">
+                  default header
+                </slot>
+            </div>
+
+            <div class="vue-modal-body">
+                <slot name="content">
+                  default content
+                </slot>
+            </div>
+
+        </div>
+      </div>
+    </div>
+  </transition>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.vue-modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, .5);
+  display: table;
+  transition: opacity .3s ease;
+}
+
+.vue-modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.vue-modal-container {
+  width: 60%;
+  margin: 0px auto;
+  padding: 20px 30px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+  transition: all .3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+}
+.vue-modal-header {
+  border-bottom: 2px solid black;
+  margin-bottom: 10px;
+}
+.vue-modal-header h3 {
+  margin-top: 0;
+  color: green;
+}
+
+.vue-modal-body {
+  margin: 20px 0;
+}
+
+.vue-modal-default-button {
+  float: right;
+}
+
+/*
+ * The following styles are auto-applied to elements with
+ * transition="modal" when their visibility is toggled
+ * by Vue.js.
+ *
+ * You can easily play with the modal transition by editing
+ * these styles.
+ */
+
+.vue-modal-enter {
+  opacity: 0;
+}
+
+.vue-modal-leave-active {
+  opacity: 0;
+}
+
+.vue-modal-enter .vue-modal-container,
+.vue-modal-leave-active .vue-modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
